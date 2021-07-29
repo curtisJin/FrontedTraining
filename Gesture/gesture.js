e4449e1ac42a406f4cd42b70b06ea526c3c50dd2
curtisJin <1059881679@qq.com>
2021-07-29 23:08:55 +0800

    completed gesture class 10

--- Gesture/gesture.js
+++ Gesture/gesture.js
@@ -1,41 +1,76 @@
 let element = document.documentElement; //Gets a reference to the root node of the document.
 
-console.log(element);
+let isListeningMouse = false;
 
 //PC
 element.addEventListener('mousedown', event => {
-  start(event);
-
+  let context = Object.create(null);
+  contexts.set('mouse'+ (1 << event.button), context);
+  start(event, context);
+  
   let mouseMove = event => {
-    move(event);
+    //The PC uses a mask to record which mouse buttons are pressed
+    let button = 1;
+    while(button <= event.buttons) {
+      if(button && event.buttons) {
+        let key;
+        if(event.buttons === 2) {
+          key = 4;
+        } else if(event.buttons === 4) {
+          key = 2;
+        } else {
+          key = button;
+        }
+        let context = contexts.get('mouse' + key);
+        move(event, context);
+      }
+      button = button << 1; //Recycle five masks
+    }
   }
 
   let mouseUp = event => {
-    end(event);
-    element.removeEventListener('mousemove', mouseMove);
-    element.removeEventListener('mouseup', mouseUp);
+    let context = contexts.get('mouse' + (1 << event.button));
+    end(event, context);
+    contexts.delete('mouse' + (1 << event.button));
+
+    if(event.buttons === 0) {
+      document.removeEventListener('mousemove', mouseMove);
+      document.removeEventListener('mouseup', mouseUp);
+      isListeningMouse = false;
+    }
   }
 
-  element.addEventListener('mousemove', mouseMove);
-  element.addEventListener('mouseup', mouseUp);
+  if(!isListeningMouse) {
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
+    isListeningMouse = true;
+  }
 })
 
 //mobile
+let contexts = new Map();
+
 element.addEventListener('touchstart', event => {
   for(let touch of event.changedTouches) {
-    start(touch);
+    //We distinguish the point created by the event by creating a context
+    let context = Object.create(null);
+    contexts.set(touch.identifier, context);
+    start(touch, context);
   }
 })
 
 element.addEventListener('touchmove', event => {
   for(let touch of event.changedTouches) {
-    move(touch);
+    let context = contexts.get(touch.identifier);
+    move(touch, context);
   }
 })
 
 element.addEventListener('touchend', event => {
   for(let touch of event.changedTouches) {
-    end(touch);
+    let context = contexts.get(touch.identifier);
+    end(touch, context);
+    contexts.delete(touch.identifier);
   }
 })
 
@@ -47,18 +82,67 @@ element.addEventListener('touchcancel', event => {
 })
 
 //Abstract events on PC and Mobile
-let start = (e) => {
-  console.log('start', e);
+let handler;
+let startX, startY;
+let isPan = false;
+let isTap = true;
+let isPress = false;
+
+let start = (e, context) => {
+  
+  context.startX = e.clientX, context.startY = e.clientY;
+  context.isPan = false;
+  context.isTap = true;
+  context.isPress = false;
+  context.handler = setTimeout(() => {
+    context.isPan = false;
+    context.isTap = false;
+    context.isPress = true;
+    context.handler = null;
+    console.log('press');
+  }, 500);
 }
 
-let move = (e) => {
-  console.log('move', e);
+let move = (e, context) => {
+  let dx = e.clientX - context.startX, dy = e.clientY - context.startY;
+
+  if(!context.isPan && dx ** 2 + dy ** 2 > 100) {
+    context.isPan = true;
+    context.isTap = false;
+    context.isPress = false;
+    console.log('panStart');
+    clearTimeout(context.handler);
+  }
+
+  if(context.isPan) {
+    console.log(dx, dy);
+    console.log('pan');
+  }
+
 }
 
-let end = (e) => {
-  console.log('end', e);
+let end = (e, context) => {
+  if(context.isTap) {
+    dispatch('tap', {});
+    clearTimeout(context.handler);
+  } else if(context.isPan) {
+    console.log('panEnded');
+  } else if(context.isPress) {
+    console.log('pressend');
+  }
 }
 
-let cancel = (e) => {
-  console.log('cancel', e);
+let cancel = (e, context) => {
+  clearTimeout(context.handler);
+}
+
+
+function dispatch (type, properties) {
+  let event = new Event(type);
+  
+  for(let name in properties) {
+    event[name] = properties[name];
+  }
+
+  element.dispatchEvent(event);
 }
